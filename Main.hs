b1837a89605834ab21860abaea4e0cefb53c57e0
James Deery <james.deery@jadex.org>
2014-12-08 20:42:10 +0000

    It's works!

--- Main.hs
+++ Main.hs
@@ -5,31 +5,85 @@ module Main where
 import Network.Connection
 import qualified Data.ByteString as B
 import Data.List.Split (splitOn)
+import Data.ByteString.Char8 (unpack, pack)
 
 connParams = ConnectionParams {  connectionHostname = "10.112.156.136"
                         , connectionPort = 8080
                         , connectionUseSecure = Nothing
                         , connectionUseSocks = Nothing }
 
-data Compass = Compass Int Int Int Int (Maybe Int) deriving (Show, Eq)
+data Compass = Compass { north :: Int
+                       , east :: Int
+                       , south :: Int
+                       , west :: Int
+                       , present :: (Maybe Move)
+                       } deriving (Show, Read, Eq)
+
+data Move = N | S | E | W | Done deriving (Show, Read, Eq)
 
 parseDirs :: String -> Compass
 parseDirs input =
         let [_:n,_:e,_:s,_:w,_:p] = splitOn " " input
             p' = case p of
                      "?" -> Nothing
-                     "X" -> Just 0
+                     "X" -> Just Done 
                      _ -> Just (read p)
         in Compass (read n) (read e) (read s) (read w) p'
 
+
+makeMove :: Move -> Compass -> Maybe Move
+makeMove _ (Compass _ _ _ _ (Just Done)) = Nothing
+makeMove _ (Compass _ _ _ _ (Just m)) = Just m
+
+makeMove N c
+  | west c > 0 = Just W
+  | north c > 0 = Just N
+  | east c > 0 = Just E
+  | south c > 0 = Just S
+  | otherwise = error "Trapped! :("
+
+makeMove W c
+  | south c > 0 = Just S
+  | west c > 0 = Just W
+  | north c > 0 = Just N
+  | east c > 0 = Just E
+  | otherwise = error "Trapped! :("
+
+makeMove S c
+  | east c > 0 = Just E
+  | south c > 0 = Just S
+  | west c > 0 = Just W
+  | north c > 0 = Just N
+  | otherwise = error "Trapped! :("
+
+makeMove E c
+  | north c > 0 = Just N
+  | east c > 0 = Just E
+  | south c > 0 = Just S
+  | west c > 0 = Just W
+  | otherwise = error "Trapped! :("
+
+
+mainLoop :: (Connection, Move) -> IO (Connection, Move)
+mainLoop (conn, oldMove) = do
+        msg <- connectionGetLine 4096 conn
+        putStrLn $ unpack msg
+        let compass = parseDirs $ unpack msg
+        case makeMove oldMove compass of
+            Just move -> do
+                connectionPut conn $ pack $ show move
+                connectionPut conn "\n"
+                mainLoop (conn, move)
+            Nothing -> do
+                putStrLn "Done!!!"
+                return (conn, Done)
 
 --main :: IO String
 main = do
         ctx <- initConnectionContext
         conn <- connectTo ctx connParams
         greeting <- connectionGetLine 4096 conn
-        putStrLn $ show greeting
+        putStrLn $ unpack greeting
         connectionPut conn "haskell\n"
-        nextMsg <- connectionGetLine 4096 conn
-        putStrLn $ show nextMsg
+        mainLoop (conn, N)
 
