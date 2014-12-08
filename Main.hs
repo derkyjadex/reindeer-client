e7214ab51c535f304532ab381114137b4d677fe9
James Deery <james.deery@jadex.org>
2014-12-08 20:52:48 +0000

    Random tidying up

--- Main.hs
+++ Main.hs
@@ -7,29 +7,29 @@ import qualified Data.ByteString as B
 import Data.List.Split (splitOn)
 import Data.ByteString.Char8 (unpack, pack)
 
-connParams = ConnectionParams {  connectionHostname = "10.112.156.136"
-                        , connectionPort = 8080
-                        , connectionUseSecure = Nothing
-                        , connectionUseSocks = Nothing }
+connParams = ConnectionParams { connectionHostname = "10.112.156.136"
+                              , connectionPort = 8080
+                              , connectionUseSecure = Nothing
+                              , connectionUseSocks = Nothing
+                              }
 
 data Compass = Compass { north :: Int
                        , east :: Int
                        , south :: Int
                        , west :: Int
-                       , present :: (Maybe Move)
+                       , present :: Maybe Move
                        } deriving (Show, Read, Eq)
 
 data Move = N | S | E | W | Done deriving (Show, Read, Eq)
 
-parseDirs :: String -> Compass
-parseDirs input =
+parseMsg :: String -> Compass
+parseMsg input =
         let [_:n,_:e,_:s,_:w,_:p] = splitOn " " input
             p' = case p of
                      "?" -> Nothing
                      "X" -> Just Done 
                      _ -> Just (read p)
         in Compass (read n) (read e) (read s) (read w) p'
-
 
 makeMove :: Move -> Compass -> Maybe Move
 makeMove _ (Compass _ _ _ _ (Just Done)) = Nothing
@@ -40,35 +40,32 @@ makeMove N c
   | north c > 0 = Just N
   | east c > 0 = Just E
   | south c > 0 = Just S
-  | otherwise = error "Trapped! :("
 
 makeMove W c
   | south c > 0 = Just S
   | west c > 0 = Just W
   | north c > 0 = Just N
   | east c > 0 = Just E
-  | otherwise = error "Trapped! :("
 
 makeMove S c
   | east c > 0 = Just E
   | south c > 0 = Just S
   | west c > 0 = Just W
   | north c > 0 = Just N
-  | otherwise = error "Trapped! :("
 
 makeMove E c
   | north c > 0 = Just N
   | east c > 0 = Just E
   | south c > 0 = Just S
   | west c > 0 = Just W
-  | otherwise = error "Trapped! :("
 
+makeMove _ _ = error "Trapped! :("
 
 mainLoop :: (Connection, Move) -> IO (Connection, Move)
 mainLoop (conn, oldMove) = do
         msg <- connectionGetLine 4096 conn
         putStrLn $ unpack msg
-        let compass = parseDirs $ unpack msg
+        let compass = parseMsg $ unpack msg
         case makeMove oldMove compass of
             Just move -> do
                 connectionPut conn $ pack $ show move
@@ -78,7 +75,6 @@ mainLoop (conn, oldMove) = do
                 putStrLn "Done!!!"
                 return (conn, Done)
 
---main :: IO String
 main = do
         ctx <- initConnectionContext
         conn <- connectTo ctx connParams
